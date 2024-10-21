06d72ea3d1099db5bbe09b6a12e49b216f64754b
Felix Neubauer <felix@neuby.de>
2024-10-21 13:48:18 +0200

    try to fix

--- website/src/main.ts
+++ website/src/main.ts
@@ -5,9 +5,7 @@ import App from './App.vue'
 import router from './router'
 
 import PrimeVue from 'primevue/config'
-//import 'primevue/resources/themes/lara-light-indigo/theme.css';
 import Aura from '@primevue/themes/aura'
-//import 'primevue/resources/primevue.min.css';
 
 const app = createApp(App)
 
